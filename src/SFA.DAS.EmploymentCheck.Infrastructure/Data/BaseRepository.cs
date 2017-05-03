3f44235a7fbaf89a1907a6d61ad2fc5b2c0fc883
db-steele <db.steele@gmail.com>
2017-05-03 12:49:44 +0100

    no message

---
+++ src/SFA.DAS.EmploymentCheck.Infrastructure/Data/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmploymentCheck.Infrastructure.Data
+{
+    public abstract class BaseRepository
+    {
+        private readonly string _connectionString;
+
+        protected BaseRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected async Task<T> WithConnection<T>(Func<IDbConnection, Task<T>> getData)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                return await getData(connection);
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.EmploymentCheck.Infrastructure/Data/EmploymentCheckRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmploymentCheck.Infrastructure.Data
+{
+    public class EmploymentCheckRepository: BaseRepository 
+    {
+        public EmploymentCheckRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<T> GetEmploymentCheck<T>(int NINumber)
+        {
+            throw new NotImplementedException();
+
+            //var result = await WithConnection(async c =>
+            //{
+            //    //var parameters = new DynamicParameters();
+            //    //parameters.Add("@eventFeed", eventFeed, DbType.String);
+
+
+            //        return await c.QuerySingleOrDefaultAsync<T>(
+            //        sql: "[Data_Load].[GetLastProcessedEventId]",
+            //        param: parameters,
+            //        commandType: CommandType.StoredProcedure);
+
+            //});
+            //return result;
+        }
+
+        public async Task StoreEmploymentCheck<T>(string eventFeed, T id)
+        {
+            throw new NotImplementedException();
+
+            //await WithConnection(async c =>
+            //{
+            //    var parameters = new DynamicParameters();
+            //    parameters.Add("@eventFeed", eventFeed, DbType.String);
+            //    parameters.Add("@lastProcessedEventId", id, DbType.String);
+
+            //    return await c.ExecuteAsync(
+            //        sql: "[Data_Load].[StoreLastProcessedEventId]",
+            //        param: parameters,
+            //        commandType: CommandType.StoredProcedure);
+            //});
+        }
+    }
+}
